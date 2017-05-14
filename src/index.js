c12442c19c8362ed9335cb822d41ba118b354e46
olehedza <olehedza@gmail.com>
2017-05-14 23:38:25 +0300

    Create less file

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@
  */
 var str = require('./constant');
 var $ = require('jquery');
+require('./less/index');
 
 var App = function() {
     console.log(str);
-    $('body').html(str);
+    $('body').html('<div class="b-test"><h3 class="b-test__heading">Header <div> </div></h3></div>');
 };
 
 var app = new App();
--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,14 @@ module.exports = {
   output: {
       path: __dirname + '/public',
       filename: 'bundle.js'
+  },
+
+  module: {
+      loaders: [
+          {
+              test: /\.less$/,
+              loader: 'style-loader!css-loader!less-loader'
+          }
+      ]
   }
 };
